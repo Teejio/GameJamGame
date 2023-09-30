386423d09834149c48d166abd6ac9a40c74a016a
ItsCapp <93293747+ItsCapp@users.noreply.github.com>
2023-09-30 02:32:31 +0100

    camera jitter unjitterer
    I interpolated a small bit for the camera so the jitter isn't as noticeable

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,15 +1,13 @@
 package;
 
-import flixel.FlxState;
-import flash.display.Stage;
 import Player;
+import flash.display.Stage;
+import flixel.FlxState;
 import lime.app.Application;
 
 class PlayState extends FlxState
 {
-
 	public var player:Player;
-
 
 	var GAMECAM:FlxCamera;
 	var HUDCAM:FlxCamera;
@@ -30,55 +28,57 @@ class PlayState extends FlxState
 
 		HUDCAM.bgColor.alpha = 0;
 		PAUSECAM.bgColor.alpha = 0;
-	
+
 		FlxG.cameras.reset(GAMECAM);
 		FlxG.cameras.add(HUDCAM, false);
 		FlxG.cameras.add(PAUSECAM, false);
 
 		GAMECAM.antialiasing = false;
 
-		player = new Player((stage.fullScreenWidth) -  (Application.current.window.width/2) , (stage.fullScreenHeight) - (Application.current.window.height/2 ));
+		player = new Player((stage.fullScreenWidth) - (Application.current.window.width / 2),
+			(stage.fullScreenHeight) - (Application.current.window.height / 2));
 		add(player);
 
-		var dickhead = new FlxSprite(player.x,player.y).makeGraphic(100,100, FlxColor.WHITE);
+		var dickhead = new FlxSprite(player.x, player.y).makeGraphic(100, 100, FlxColor.WHITE);
 		add(dickhead);
-		GAMECAM.follow(player, 1);
+		// GAMECAM.follow(player, FlxCameraFollowStyle.NO_DEAD_ZONE);
 
 		gameWidth = Application.current.window.display.bounds.width;
 		gameHeight = Application.current.window.display.bounds.height;
-
-		
 	}
 
 	override public function update(elapsed:Float)
 	{
+		// player.x = Math.round( player.x);
+		// player.y = Math.round( player.y);
 
-
-
-
-	
-		//player.x = Math.round( player.x);
-		//player.y = Math.round( player.y);
 		super.update(elapsed);
 
-		Application.current.window.x = Std.int((gameWidth/-2) +player.x);
-		
-		Application.current.window.y = Std.int((gameHeight/-2) + player.y );
-
+		windowUpdate();
 
 		trace(Application.current.window.x);
+		trace(Application.current.window.y);
 
-		if (FlxG.keys.anyPressed([X, ESCAPE])) {
-
+		if (FlxG.keys.anyPressed([X, ESCAPE]))
+		{
 			Application.current.window.close();
 		}
 	}
 
+	function windowUpdate()
+	{
+		var desiredCamPosX = Std.int(player.x + (FlxG.width / -2));
+		var desiredCamPosY = Std.int(player.y + (FlxG.height / -2));
+
+		GAMECAM.scroll.x = FlxMath.lerp(GAMECAM.scroll.x, desiredCamPosX, 0.75);
+		GAMECAM.scroll.y = FlxMath.lerp(GAMECAM.scroll.y, desiredCamPosY, 0.75);
+
+		Application.current.window.x = Std.int((gameWidth / -2) + player.x);
+		Application.current.window.y = Std.int((gameHeight / -2) + player.y);
+	}
+
 	override public function draw()
-
-		{
-			super.draw();
-
-			
-		}
+	{
+		super.draw();
+	}
 }
